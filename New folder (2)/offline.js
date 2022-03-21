948c4e5b0d9c3aa9da3df03ced754bb3f3e088ac
jitti-s54 <34131737+jitti-s54@users.noreply.github.com>
2022-03-21 19:33:17 +0700

    Add files via upload
    test

---
+++ New folder (2)/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1647865889,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/enemybullet-sheet0.png",
+		"images/star-sheet0.png",
+		"images/enemy2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
